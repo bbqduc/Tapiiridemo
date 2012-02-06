4e0ed4a6a52a4410b7b247733455a027e5090f26
bbqduc <brunoankka@gmail.com>
2012-02-06 21:01:53 +0200

    draw triangle

--- main.c
+++ main.c
@@ -4,6 +4,21 @@
 #include <stdio.h>
 #include "shader.h"
 
+
+GLfloat vertices[] = {-1.0f, -1.0f, 0.0f,
+					1.0f, -1.0f, 0.0f,
+					0.0f, 1.0f, 0.0f};
+GLuint vertexBuffer;
+
+void checkGLErrors(const char* functionName){
+	GLenum err = glGetError();
+	if(err != GL_NO_ERROR)
+	{
+		fprintf(stderr, "%s -- ERROR: %s\n", functionName, gluErrorString(err));
+		exit(-1);
+	}
+}
+
 int init()
 {
 	if( !glfwInit()) 	
@@ -30,7 +45,34 @@ int init()
 		printf("opengl 3.2 is not supported\n");
 		return -1;
 	}
+	GLuint VertexArrayID;
+	glGenVertexArrays(1, &VertexArrayID);
+	glBindVertexArray(VertexArrayID);
+	
+	glGenBuffers(1, &vertexBuffer);
+
+	glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), &vertices[0], GL_STATIC_DRAW);
+
+	checkGLErrors("init");
+
 	return 0;
+}
+
+void drawSimpleTriangle(const Shader* shader)
+{
+	glUseProgram(shader->id);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+
+	glDisableVertexAttribArray(0);
+	glUseProgram(0);
+	checkGLErrors("drawSimpleTriangle");
 }
 
 int main()
@@ -44,10 +86,11 @@ int main()
 
 	Shader plain;
 	shaderInitialize(&plain, "shaders/plain.vert", "shaders/plain.frag", "");
-
+	checkGLErrors("beforemainloop");
 	while(running)
 	{
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		drawSimpleTriangle(&plain);
 		glfwSwapBuffers();
 		running = !glfwGetKey(GLFW_KEY_ESC) && glfwGetWindowParam(GLFW_OPENED);
 	}
