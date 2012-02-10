65586ad30672ccaeb3ee4cb9773580db30d8d944
Detegr <detegr@gmail.com>
2012-02-10 15:12:25 +0200

    Removed useless namespaces

--- concurrency.cpp
+++ concurrency.cpp
@@ -4,148 +4,145 @@
 	#include <process.h>
 #endif
 
-namespace pwskoag
+#ifdef _WIN32
+	unsigned __stdcall C_Thread::M_ThreadInit(void* args)
+	{
+		C_Thread::C_Data* d=(C_Thread::C_Data*)args;
+		d->m_Func(d->m_Arg);
+		return 0;
+	}
+	C_Thread::C_Thread(t_ThreadFunc f, void* args) : m_Thread(0), m_Data(C_Data(f, args))
+	{
+		m_Thread=(HANDLE)_beginthreadex(NULL,0,M_ThreadInit,&m_Data,0,NULL);
+		if(!m_Thread)
+		{
+			throw std::runtime_error("Error creating a thread.");
+		}
+	}
+#else
+	void* C_Thread::M_ThreadInit(void* args)
+	{
+		C_Thread::C_Data* d=(C_Thread::C_Data*)args;
+		d->m_Func(d->m_Arg);
+		return NULL;
+	}
+	C_Thread::C_Thread(t_ThreadFunc f, void* args) : m_Thread(0), m_Data(C_Data(f, args))
+	{
+		int ret=pthread_create(&m_Thread, NULL, M_ThreadInit, &m_Data);
+		if(ret)
+		{
+			throw std::runtime_error("Error creating a thread.");
+		}
+	}
+	void C_Thread::M_Start(t_ThreadFunc f, void* args)
+	{
+		m_Thread=0;
+		m_Data=C_Data(f, args);
+		int ret=pthread_create(&m_Thread, NULL, M_ThreadInit, &m_Data);
+		if(ret)
+		{
+			throw std::runtime_error("Error creating a thread.");
+		}
+	}
+#endif
+
+void C_Thread::M_Join()
 {
 	#ifdef _WIN32
-		unsigned __stdcall C_Thread::M_ThreadInit(void* args)
+		WaitForSingleObject(m_Thread, INFINITE);
+	#else
+		pthread_join(m_Thread, NULL);
+	#endif
+}
+C_Thread::~C_Thread()
+{
+	#ifdef _WIN32
+		if(m_Thread)
 		{
-			C_Thread::C_Data* d=(C_Thread::C_Data*)args;
-			d->m_Func(d->m_Arg);
-			return 0;
-		}
-		C_Thread::C_Thread(t_ThreadFunc f, void* args) : m_Thread(0), m_Data(C_Data(f, args))
-		{
-			m_Thread=(HANDLE)_beginthreadex(NULL,0,M_ThreadInit,&m_Data,0,NULL);
-			if(!m_Thread)
-			{
-				throw std::runtime_error("Error creating a thread.");
-			}
+			M_Join();
+			CloseHandle(m_Thread);
+			m_Thread=NULL;
 		}
 	#else
-		void* C_Thread::M_ThreadInit(void* args)
+		if(m_Thread)
 		{
-			C_Thread::C_Data* d=(C_Thread::C_Data*)args;
-			d->m_Func(d->m_Arg);
-			return NULL;
-		}
-		C_Thread::C_Thread(t_ThreadFunc f, void* args) : m_Thread(0), m_Data(C_Data(f, args))
-		{
-			int ret=pthread_create(&m_Thread, NULL, M_ThreadInit, &m_Data);
-			if(ret)
-			{
-				throw std::runtime_error("Error creating a thread.");
-			}
-		}
-		void C_Thread::M_Start(t_ThreadFunc f, void* args)
-		{
+			M_Join();
+			pthread_detach(m_Thread);
 			m_Thread=0;
-			m_Data=C_Data(f, args);
-			int ret=pthread_create(&m_Thread, NULL, M_ThreadInit, &m_Data);
-			if(ret)
-			{
-				throw std::runtime_error("Error creating a thread.");
-			}
 		}
 	#endif
+}
 
-	void C_Thread::M_Join()
-	{
-		#ifdef _WIN32
-			WaitForSingleObject(m_Thread, INFINITE);
-		#else
-			pthread_join(m_Thread, NULL);
-		#endif
-	}
-	C_Thread::~C_Thread()
-	{
-		#ifdef _WIN32
-			if(m_Thread)
-			{
-				M_Join();
-				CloseHandle(m_Thread);
-				m_Thread=NULL;
-			}
-		#else
-			if(m_Thread)
-			{
-				M_Join();
-				pthread_detach(m_Thread);
-				m_Thread=0;
-			}
-		#endif
-	}
+C_Mutex::C_Mutex()
+{
+	#ifdef _WIN32
+		InitializeCriticalSection(&m_Mutex);
+	#else
+		pthread_mutexattr_t attr;
+		pthread_mutexattr_init(&attr);
+		pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_RECURSIVE);
+		pthread_mutex_init(&m_Mutex, &attr);
+		pthread_mutexattr_destroy(&attr);
+	#endif
+}
+C_Mutex::~C_Mutex()
+{
+	#ifdef _WIN32
+		DeleteCriticalSection(&m_Mutex);
+	#else
+		pthread_mutex_destroy(&m_Mutex);
+	#endif
+}
+void C_Mutex::M_Lock()
+{
+	#ifdef _WIN32
+		EnterCriticalSection(&m_Mutex);
+	#else
+		pthread_mutex_lock(&m_Mutex);
+	#endif
+}
+void C_Mutex::M_Unlock()
+{
+	#ifdef _WIN32
+		LeaveCriticalSection(&m_Mutex);
+	#else
+		pthread_mutex_unlock(&m_Mutex);
+	#endif
+}
 
-	C_Mutex::C_Mutex()
-	{
-		#ifdef _WIN32
-			InitializeCriticalSection(&m_Mutex);
-		#else
-			pthread_mutexattr_t attr;
-			pthread_mutexattr_init(&attr);
-			pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_RECURSIVE);
-			pthread_mutex_init(&m_Mutex, &attr);
-			pthread_mutexattr_destroy(&attr);
-		#endif
-	}
-	C_Mutex::~C_Mutex()
-	{
-		#ifdef _WIN32
-			DeleteCriticalSection(&m_Mutex);
-		#else
-			pthread_mutex_destroy(&m_Mutex);
-		#endif
-	}
-	void C_Mutex::M_Lock()
-	{
-		#ifdef _WIN32
-			EnterCriticalSection(&m_Mutex);
-		#else
-			pthread_mutex_lock(&m_Mutex);
-		#endif
-	}
-	void C_Mutex::M_Unlock()
-	{
-		#ifdef _WIN32
-			LeaveCriticalSection(&m_Mutex);
-		#else
-			pthread_mutex_unlock(&m_Mutex);
-		#endif
-	}
-
-	C_CondVar::C_CondVar()
-	{
-		#ifdef _WIN32
-			InitializeConditionVariable(&m_Cond);
-		#else
-			pthread_cond_init(&m_Cond,NULL);
-		#endif
-	}
-	void C_CondVar::M_Wait()
-	{
-		#ifdef _WIN32
-			m_Mutex.M_Lock();
-			SleepConditionVariableCS(&m_Cond, &m_Mutex.m_Mutex, INFINITE);
-			m_Mutex.M_Unlock();
-		#else
-			m_Mutex.M_Lock();
-			pthread_cond_wait(&m_Cond, &m_Mutex.m_Mutex);
-			m_Mutex.M_Unlock();
-		#endif
-	}
-	void C_CondVar::M_SignalOne()
-	{
-		#ifdef _WIN32
-			WakeConditionVariable(&m_Cond);
-		#else
-			pthread_cond_signal(&m_Cond);
-		#endif
-	}
-	void C_CondVar::M_Signal()
-	{
-		#ifdef _WIN32
-			WakeAllConditionVariable(&m_Cond);
-		#else
-			pthread_cond_broadcast(&m_Cond);
-		#endif
-	}
+C_CondVar::C_CondVar()
+{
+	#ifdef _WIN32
+		InitializeConditionVariable(&m_Cond);
+	#else
+		pthread_cond_init(&m_Cond,NULL);
+	#endif
+}
+void C_CondVar::M_Wait()
+{
+	#ifdef _WIN32
+		m_Mutex.M_Lock();
+		SleepConditionVariableCS(&m_Cond, &m_Mutex.m_Mutex, INFINITE);
+		m_Mutex.M_Unlock();
+	#else
+		m_Mutex.M_Lock();
+		pthread_cond_wait(&m_Cond, &m_Mutex.m_Mutex);
+		m_Mutex.M_Unlock();
+	#endif
+}
+void C_CondVar::M_SignalOne()
+{
+	#ifdef _WIN32
+		WakeConditionVariable(&m_Cond);
+	#else
+		pthread_cond_signal(&m_Cond);
+	#endif
+}
+void C_CondVar::M_Signal()
+{
+	#ifdef _WIN32
+		WakeAllConditionVariable(&m_Cond);
+	#else
+		pthread_cond_broadcast(&m_Cond);
+	#endif
 }
--- concurrency.h
+++ concurrency.h
@@ -5,79 +5,76 @@
 #endif
 #include <iostream>
 
-namespace pwskoag
+class C_Thread
 {
-	class C_Thread
-	{
-		private:
-			typedef void(*t_ThreadFunc)(void*);
+	private:
+		typedef void(*t_ThreadFunc)(void*);
 
-			#ifdef _WIN32
-				HANDLE m_Thread;
-				static unsigned __stdcall M_ThreadInit(void* args);
-			#else
-				pthread_t m_Thread;
-				static void* M_ThreadInit(void* args);
-			#endif
-			
-			struct C_Data
-			{
-				C_Data() : m_Func(NULL), m_Arg(NULL) {}
-				C_Data(t_ThreadFunc f, void* a) : m_Func(f), m_Arg(a) {}
-				t_ThreadFunc 	m_Func;
-				void*			m_Arg;
-			};
+		#ifdef _WIN32
+			HANDLE m_Thread;
+			static unsigned __stdcall M_ThreadInit(void* args);
+		#else
+			pthread_t m_Thread;
+			static void* M_ThreadInit(void* args);
+		#endif
+		
+		struct C_Data
+		{
+			C_Data() : m_Func(NULL), m_Arg(NULL) {}
+			C_Data(t_ThreadFunc f, void* a) : m_Func(f), m_Arg(a) {}
+			t_ThreadFunc 	m_Func;
+			void*			m_Arg;
+		};
 
-			C_Data m_Data;
-		public:
-			 C_Thread() : m_Thread(0) {}
-			 C_Thread(t_ThreadFunc f, void* args=NULL);
-			 void M_Start(t_ThreadFunc f, void* args=NULL);
-			 ~C_Thread();
-			 void M_Join();
-	};
+		C_Data m_Data;
+	public:
+		 C_Thread() : m_Thread(0) {}
+		 C_Thread(t_ThreadFunc f, void* args=NULL);
+		 void M_Start(t_ThreadFunc f, void* args=NULL);
+		 ~C_Thread();
+		 void M_Join();
+};
 
-	class C_Mutex
-	{
-		friend class C_CondVar;
-		private:
-			#ifdef _WIN32
-				CRITICAL_SECTION m_Mutex;
-			#else
-				pthread_mutex_t m_Mutex;
-			#endif
-		public:
-			 C_Mutex();
-			 ~C_Mutex();
-			 void M_Lock();
-			 void M_Unlock();
-	};
+class C_Mutex
+{
+	friend class C_CondVar;
+	private:
+		#ifdef _WIN32
+			CRITICAL_SECTION m_Mutex;
+		#else
+			pthread_mutex_t m_Mutex;
+		#endif
+	public:
+		 C_Mutex();
+		 ~C_Mutex();
+		 void M_Lock();
+		 void M_Unlock();
+};
 
-	class C_Lock
-	{
-		private:
-			C_Mutex* m_Mutex;
-		public:
-			 C_Lock(C_Mutex& m) : m_Mutex(&m) {m_Mutex->M_Lock();}
-			 ~C_Lock() {m_Mutex->M_Unlock();}
-	};
+class C_Lock
+{
+	private:
+		C_Mutex* m_Mutex;
+	public:
+		 C_Lock(C_Mutex& m) : m_Mutex(&m) {m_Mutex->M_Lock();}
+		 ~C_Lock() {m_Mutex->M_Unlock();}
+};
 
-	class C_CondVar
-	{
-		private:
-			#ifdef _WIN32
-				CONDITION_VARIABLE m_Cond;
-			#else
-				pthread_cond_t m_Cond;
-			#endif
-			C_Mutex m_Mutex;
-		public:
-			 C_CondVar();
-			#ifndef _WIN32
-				~C_CondVar() {pthread_cond_destroy(&m_Cond);}
-			#endif
-			 void M_Wait();
-			 void M_SignalOne();
-			 void M_Signal();
-	};
-}
+class C_CondVar
+{
+	private:
+		#ifdef _WIN32
+			CONDITION_VARIABLE m_Cond;
+		#else
+			pthread_cond_t m_Cond;
+		#endif
+		C_Mutex m_Mutex;
+	public:
+		 C_CondVar();
+		#ifndef _WIN32
+			~C_CondVar() {pthread_cond_destroy(&m_Cond);}
+		#endif
+		 void M_Wait();
+		 void M_SignalOne();
+		 void M_Signal();
+};
